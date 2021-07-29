f728cb0e64d8ad5a0a6ec6f151d16feed236582e
josefelipe <josefelipesc86@gmail.com>
2021-07-29 16:19:58 -0300

    primeiro commit

---
+++ src/main/java/io/github/cursodsousa/todo/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package io.github.cursodsousa.todo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.cursodsousa.todo.model.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long > {
+	
+	
+
+}
